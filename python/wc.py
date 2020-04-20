7d2858ffe31157ac77e3418949aa4118d5760fd6
Ryan Tate <ryantate13@gmail.com>
2020-04-19 19:07:25 -0700

    process raw bytes

--- python/wc.py
+++ python/wc.py
@@ -1,19 +1,20 @@
 import sys
 
-def isspace(c: str) -> bool:
-    return c == ' ' or c == '\t' or c == '\v' or c == '\f' or c == '\r'
+spaces: set = set(map(ord, [' ', '\n', '\t', '\v', '\f', '\r']))
+
+def isspace(b: bytes) -> bool:
+	return b not in spaces
 
 
 word_count: int = 0
+in_space: bool = True
 
-for line in sys.stdin:
-	in_space: bool = True
-	for c in line:
-		if isspace(c):
-			in_space = True
-		else:
-			if in_space:
-				word_count += 1
-			in_space = False
+for b in sys.stdin.buffer.read():
+	if isspace(b):
+		in_space = True
+	else:
+		if in_space:
+			word_count += 1
+		in_space = False
 
 print(word_count)
--- rust/wc.rs
+++ rust/wc.rs
@@ -1,29 +1,22 @@
 use std::io;
-use std::io::BufRead;
+use std::io::Read;
 
 fn isspace(c: char) -> bool {
-	return c == ' '
-		|| c == '\t'
-		|| c == '\x0B'
-		|| c == '\x0C'
-		|| c == '\r';
+    return c == ' ' || c == '\n' || c == '\t' || c == '\x0B' || c == '\x0C' || c == '\r';
 }
 
-fn main(){
-	let mut word_count: u32 = 0;
-	for line in io::stdin().lock().lines() {
-		let mut in_space: bool = true;
-		for c in line.unwrap().chars() {
-			if isspace(c) {
-				in_space = true;
-			}
-			else {
-				if in_space {
-					word_count += 1;
-				}
-				in_space = false;
-			}
-		}
-	}
-	println!("{:?}", word_count);
+fn main() {
+    let mut word_count: u32 = 0;
+    let mut in_space: bool = true;
+    for c in io::stdin().lock().bytes() {
+        if isspace(c.unwrap().into()) {
+            in_space = true;
+        } else {
+            if in_space {
+                word_count += 1;
+            }
+            in_space = false;
+        }
+    }
+    println!("{:?}", word_count);
 }
